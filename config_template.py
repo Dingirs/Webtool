992e1cf125c27aa1cec7d84fb358d0cf82f41cfb
Dingirs <1612326488@qq.com>
2023-06-16 15:01:47 -0400

    add template

---
+++ config_template.py
@@ -0,0 +1,15 @@
+
+#AZURE
+CLIENT_ID = 'ID'
+CLIENT_SECRET = 'SECRET'
+AUTHORITY = 'https://login.microsoftonline.com/common'
+TENANT_ID = 'common'
+SCOPE = ['User.Read', 'Files.ReadWrite.All']
+
+#CHATGPT
+CHATGPT_API_KEY = 'key'
+
+
+#FLASK
+SECRET_KEY = 'KEY'
+SESSION_TYPE = "filesystem"
